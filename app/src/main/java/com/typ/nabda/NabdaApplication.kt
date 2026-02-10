af8614c11e97b105791793b4aee11eda73cc85d1
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-02-10 14:03:39 +0200

    feat(app): Register Koin modules for features and core components
    This commit updates the `NabdaApplication` class to register several new Koin modules, integrating core functionalities and feature modules into the dependency injection graph.
    
    Specifically, the following changes have been made:
    
    *   **Module Registration:**
        *   New Koin modules have been added to the `startKoin` configuration for the following features and components:
            *   `pairingFeatureModule`
            *   `dispatcherModule`
            *   `deafBlindFeatureModule`
            *   `caregiverFeatureModule`
        *   The existing module imports have been cleaned up to use direct references.
    
    *   **Firebase Initialization:**
        *   The call to `initializeFirebase(this)` has been commented out, suggesting a temporary suspension or relocation of Firebase initialization.

--- app/src/main/java/com/typ/nabda/NabdaApplication.kt
+++ app/src/main/java/com/typ/nabda/NabdaApplication.kt
@@ -1,6 +1,13 @@
 package com.typ.nabda
 
 import android.app.Application
+import com.typ.nabda.core.dispatcher.di.dispatcherModule
+import com.typ.nabda.core.notifications.di.notificationsModule
+import com.typ.nabda.feature.caregiver.di.caregiverFeatureModule
+import com.typ.nabda.feature.deafblind.di.deafBlindFeatureModule
+import com.typ.nabda.feature.pairing.di.pairingFeatureModule
+import com.typ.nabda.infrastructure.fcm.di.fcmModule
+import com.typ.nabda.infrastructure.storage.di.storageModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -8,12 +15,18 @@ class NabdaApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+//        initializeFirebase(this)
+
         startKoin {
             androidContext(this@NabdaApplication)
             modules(
-                com.typ.nabda.core.notifications.di.notificationsModule,
-                com.typ.nabda.infrastructure.storage.di.storageModule,
-                com.typ.nabda.infrastructure.fcm.di.fcmModule
+                notificationsModule,
+                storageModule,
+                fcmModule,
+                pairingFeatureModule,
+                dispatcherModule,
+                deafBlindFeatureModule,
+                caregiverFeatureModule
             )
         }
     }
