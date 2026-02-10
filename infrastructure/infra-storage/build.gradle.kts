959e763048cda90fd7eb7bc0ad9de6069a9f17cd
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-02-10 13:07:35 +0200

    feat(deps): Add DataStore Preferences dependency
    This commit adds the Jetpack DataStore Preferences library as a dependency to the `infra-storage` module.
    
    Specifically, the following changes have been made:
    
    *   **`gradle/libs.versions.toml`**:
        *   A new entry, `androidx-datastore-preferences`, has been added to the `[libraries]` section, pointing to `androidx.datastore:datastore-preferences`.
        *   A corresponding version reference, `datastore = "1.0.0"`, has been defined in the `[versions]` section.
    
    *   **`infrastructure/infra-storage/build.gradle.kts`**:
        *   The newly defined `libs.androidx.datastore.preferences` dependency has been added to the `implementation` configuration.

--- infrastructure/infra-storage/build.gradle.kts
+++ infrastructure/infra-storage/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation(project(":core:common"))
     implementation(project(":core:pairing"))
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.androidx.datastore.preferences)
     
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
