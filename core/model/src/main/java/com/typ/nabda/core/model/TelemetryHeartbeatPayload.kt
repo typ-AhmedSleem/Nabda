a57b9cca8ca28f395b800ca94a9adc9436e78d4c
typ-AhmedSleem <theyoungprogrammer67@gmail.com>
2026-03-09 15:41:46 +0200

    ref(core): Make battery and charging fields optional in TelemetryHeartbeatPayload
    This commit updates the `TelemetryHeartbeatPayload` data class to allow nullable values for battery-related fields. This change ensures the model can handle cases where battery information might be unavailable during telemetry collection.
    
    Specifically, the following changes have been made:
    
    *   **`TelemetryHeartbeatPayload.kt`**:
        *   Changed `batteryPercentage` type from `Int` to `Int?`.
        *   Changed `isCharging` type from `Boolean` to `Boolean?`.

--- core/model/src/main/java/com/typ/nabda/core/model/TelemetryHeartbeatPayload.kt
+++ core/model/src/main/java/com/typ/nabda/core/model/TelemetryHeartbeatPayload.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class TelemetryHeartbeatPayload(
     val deviceId: String,
-    val batteryPercentage: Int,
+    val batteryPercentage: Int?,
     val connectivitySource: ConnectivitySource,
-    val isCharging: Boolean,
+    val isCharging: Boolean?,
     val location: LocationSnapshot?,
     val timestamp: Long,
 )
